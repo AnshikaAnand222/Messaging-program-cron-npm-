5ceba5529755d959ebdb724bc4a58bf6273623ba
AnshikaAnand222 <73892943+AnshikaAnand222@users.noreply.github.com>
2022-02-13 11:32:00 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ACCOUNT_SID : 'AC5a388d2c1b858bf56b953d72ea98e9f0',
+    AUTH_TOKEN : '1f4760b6917bcf7bf94d36d9e493e6b5',
+    PHONE_NR : '+918655739129'
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+const cron = require('node-cron');
+
+const config = require('./config');
+const accountSid = config.ACCOUNT_SID;
+const authToken = config.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+const messages = require('./messages');
+
+const currentmessage = 0;
+
+function SendMessage() {
+    client.messages
+        .create({
+            body: messages[currentmessage],
+            from: '+18455522027',
+            to: config.PHONE_NR
+        })
+        .then(message => {
+            currentmessage++;
+        });
+}
+cron.schedule('* * * * * ', () => {
+    console.log('message sent');
+    SendMessage();
+});
